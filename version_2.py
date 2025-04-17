3be1cfa3594438f4c520eae406a912adc4b6155e
Wuyang_Gao <wuyangg@umich.edu>
2025-04-17 01:15:57 -0400

    add stuff back

--- version_2.py
+++ version_2.py
@@ -344,6 +344,23 @@ elif selected_section == "Professor overview":
             st.write('\n')
             st.write('\n')
             st.altair_chart(prof_chart)
+        
+        st.subheader("📝 Most Cited Publications")
+        st.write("Here are some of the most influential papers authored by this professor.")
+        st.dataframe(pub_df)
+        st.write('\n')
+
+        st.subheader('🧐 Research Expertise')
+        st.markdown(
+            """
+            This radar chart visualizes the **top 8 research expertise areas** of the selected professor,  
+            based on their publications, citations, and interdisciplinary reach.  
+            """
+        )
+        col1, col2 = st.columns([2,1])
+        with col1:
+            fig = profile_individual(str(selected_university), str(professor_name_input))
+            st.plotly_chart(fig, use_container_width=True)
 
     def comparison():
         st.write('ohohoh')
